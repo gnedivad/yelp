bb71ff7d0ea2f9aa1c4f9dfd021df8e4fab868cc
Ahmed <attia2096@gmail.com>
2017-03-13 21:24:12 -0700

    add adj to database

---
+++ app/create2.sql
@@ -0,0 +1,35 @@
+drop table if exists Restaurants;
+drop table if exists Categories;
+drop table if exists Reviews;
+
+create table Restaurants(
+	RestaurantID VARCHAR PRIMARY KEY,
+	Name VARCHAR,
+	City VARCHAR,
+	Latitude FLOAT,
+	Longitude FLOAT,
+	Price INTEGER,
+	Rating FLOAT
+);
+
+create table Categories(
+	RestaurantID VARCHAR NOT NULL,
+	Category VARCHAR,
+	PRIMARY KEY (RestaurantID, Category),
+	FOREIGN KEY(RestaurantID) REFERENCES Restaurants(RestaurantID)
+);
+
+create table Reviews(
+       ReviewID VARCHAR PRIMARY KEY,
+       RestaurantID VARCHAR NOT NULL,
+       Text VARCHAR,
+       Rating INTEGER,
+       FOREIGN KEY (RestaurantID) REFERENCES Restaurants(RestaurantID)
+);
+
+
+create table Adjectives(
+       RestaurantID VARCHAR NOT NULL,
+       Text VARCHAR,
+       FOREIGN KEY (RestaurantID) REFERENCES Restaurants(RestaurantID)
+);
